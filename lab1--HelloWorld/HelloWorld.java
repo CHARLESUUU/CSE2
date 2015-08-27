b29a8e7b33edbb33483e8f2504140ac8536abe4a
charlesuuu <erq219@lehigh.edu>
2015-08-27 15:36:29 +0000

    updated HelloWorld

---
+++ lab1--HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+/* This is my first java program for CSE2
+  yo
+  
+ */
+ 
+ public class HelloWorld
+ {
+     public static void main(String[] args)
+     {
+         System.out.println("$$$$$$$$$$$$$$$$$$");
+         System.out.println("$                $");
+         System.out.println("$                $");
+         System.out.println("$  Hello World!  $");
+         System.out.println("$                $");
+         System.out.println("$$$$$$$$$$$$$$$$$$");
+     }
+ }
