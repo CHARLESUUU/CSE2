92ee1863fe245bb207622199f2961eef7e2acfe1
charlesuuu <erq219@lehigh.edu>
2015-08-31 15:20:29 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,17 @@
+public class WelcomeClass 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-E--R--Q--2--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I love running and plan" 
+        +"\nto attend marathon in the fall.");
+        
+    }
+}
